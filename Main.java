9e3e1ba0de5a7ed5570b9127757fa477e863851f
LAPTOP-L6JQBG9C\Vinny <vzelinsky@albany.edu>
2016-11-14 15:12:30 -0500

    Comments out the addNode calls and adds a call to the new popList method to test. Works as intended

--- Main.java
+++ Main.java
@@ -8,10 +8,11 @@ public class Main {
   public static void main(String[] args) {
     List myList = new List();
     
-    myList.addNode();
-    myList.addNode();
-    myList.addNode();
-    myList.addNode();
+    //myList.addNode();
+    //myList.addNode();
+    //myList.addNode();
+    //myList.addNode();
+    myList.popList(5);
     
     myList.printListForward();
   }
