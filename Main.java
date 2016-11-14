a4760a0478ce76db608abe26b0a6486ef1a942eb
LAPTOP-L6JQBG9C\Vinny <vzelinsky@albany.edu>
2016-11-13 21:46:43 -0500

    Initializes Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+/* Main.java(H5)
+ * Written by Vincent Zelinsky
+ * 
+ * Contains the methods to test Stack.java and Queue.java
+ */
+public class Main {
+  //main method
+  public static void main(String[] args) {
+  }
+}
