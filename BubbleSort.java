8d017b345328d12d0272b75157b2f757f9edfc29
LAPTOP-L6JQBG9C\Vinny <vzelinsky@albany.edu>
2016-11-16 22:22:40 -0500

    Starts the creation of BubbleSort class

--- BubbleSort.java
+++ BubbleSort.java
@@ -4,4 +4,14 @@
  * Contains the necessary methods to sort data with the bubble sort algorithm
  */
 public class BubbleSort {
+  //Private fields
+  private Node currPos;
+  private Node lastPos;
+  //Constructors
+  public BubbleSort() {
+  }
+  //sortIntList method
+  //Sorts an Int
+  public void sortIntList(List listInput){
+  }
 }
