edff75bab88b868292c89316df36ad7b26e4e1a7
LAPTOP-L6JQBG9C\Vinny <vzelinsky@albany.edu>
2016-11-19 18:07:14 -0500

    Adds outer loop to BubbleSort.java that iterates n-1 times

--- BubbleSort.java
+++ BubbleSort.java
@@ -5,14 +5,21 @@
  */
 public class BubbleSort {
   //Private fields
-  private Node currPos;
-  private Node lastPos;
+  private Node t;
+  //private Node lastPos;
   //Constructors
   public BubbleSort() {
+    this.t = null;
+    //this.lastPos = null;
   }
   //sortIntList method
   //Sorts an Int
   public void sortIntList(List listInput){
     Node temp;
+    int k = 0;
+    for(int i = listInput.length()-1; i != 0; i--){
+      k++;
+    }
+    System.out.println(k);
   }
 }
