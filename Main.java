031acf76fca7c3fb750b6c9c17035b0b63642197
LAPTOP-L6JQBG9C\Vinny <vzelinsky@albany.edu>
2016-11-20 18:01:59 -0500

    Cleans up test print statements and unnecessary tests

--- Main.java
+++ Main.java
@@ -8,28 +8,20 @@ public class Main {
   public static void main(String[] args) {
     List myList = new List();
     
-    myList.addNode();
-    myList.addNode();
-    myList.addNode();
-    myList.addNode();
-    //myList.popList(3);
+    //myList.addNode();
+    //myList.addNode();
+    //myList.addNode();
+    myList.popList(10);
+    
+    System.out.println("Unsorted List:");
     myList.printListForward();
+    
     System.out.println("----------");
-    //int[] myArray;
-    //myArray = (myList.arrayOutput());
-    //System.out.println(myArray[0]);
-    //System.out.println(myArray[1]);
-    //System.out.println(myArray[2]);
-    //System.out.println(myArray[3]);
-    //myList.arrayInput(myArray);
-    
-    //myList.printListForward();
-    //myList.printListForward();
-    //System.out.println(myList.length());
-    
+ 
     BubbleSort sorter = new BubbleSort();
     sorter.sortIntList(myList);
-    //System.out.println("fuck this");
+    
+    System.out.println("Sorted List: ");
     myList.printListForward();
   }
 }
