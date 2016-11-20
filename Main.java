874fd754405737caf93659acf5a9ab8d4aee2e0a
LAPTOP-L6JQBG9C\Vinny <vzelinsky@albany.edu>
2016-11-20 02:22:53 -0500

    Tests the two new methods which work as intended

--- Main.java
+++ Main.java
@@ -8,18 +8,27 @@ public class Main {
   public static void main(String[] args) {
     List myList = new List();
     
-    //myList.addNode();
-    //myList.addNode();
-    //myList.addNode();
-    //myList.addNode();
-    myList.popList(3);
-    
+    myList.addNode();
+    myList.addNode();
+    myList.addNode();
+    myList.addNode();
+    //myList.popList(3);
     myList.printListForward();
-    System.out.println(myList.length());
+    System.out.println("----------");
+    int[] myArray;
+    myArray = (myList.arrayOutput());
+    System.out.println(myArray[0]);
+    System.out.println(myArray[1]);
+    System.out.println(myArray[2]);
+    System.out.println(myArray[3]);
+    myList.arrayInput(myArray);
+    myList.printListForward();
+    //myList.printListForward();
+    //System.out.println(myList.length());
     
-    BubbleSort sorter = new BubbleSort();
-    sorter.sortIntList(myList);
-    
+    //BubbleSort sorter = new BubbleSort();
+    //sorter.sortIntList(myList);
+    //System.out.println("fuck this");
     //myList.printListForward();
   }
 }
