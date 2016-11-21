6e1a9e6db87a3fdd826185ba4729613dfba271e1
LAPTOP-L6JQBG9C\Vinny <vzelinsky@albany.edu>
2016-11-20 21:23:23 -0500

    Fixes final bugs

--- Main.java
+++ Main.java
@@ -6,21 +6,28 @@
 public class Main {
   //main method
   public static void main(String[] args) {
+ 
     List myList = new List();
     
     //myList.addNode();
     //myList.addNode();
     //myList.addNode();
+    
+    //Populates myList with 10  elements
     myList.popList(10);
     
+    //Prints myList(unsorted) to console
     System.out.println("Unsorted List:");
     myList.printListForward();
     
     System.out.println("----------");
  
+    //Creates an instance of BubbleSort
     BubbleSort sorter = new BubbleSort();
+    //Calls sortIntList and passes myList
     sorter.sortIntList(myList);
     
+    //Prints the sorted myList to console
     System.out.println("Sorted List: ");
     myList.printListForward();
   }
